9f0dd5ea1b60312850a04e48fca87e03919a41a8
Joao Leal <a21304167@alunos.ulht.pt>
2022-10-17 12:02:18 +0100

    First commit

---
+++ CuteAnimal/Cat.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CuteAnimal
+{
+    public class Cat
+    {
+        private String color;
+        private int numOfLegs;
+        private int numOfEyes;
+
+
+        public GetColor()
+        {
+
+        }
+
+        public GetNumLegs()
+        {
+
+        }
+
+        public GetNumEyes()
+        {
+
+        }
+
+    }
+}
